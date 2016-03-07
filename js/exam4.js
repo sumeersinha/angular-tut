2b9d9a3b7adb36d2ee312c29f73ba60bb33bb011
Sumeer <sumeersinha@gmail.com>
2016-03-07 09:04:03 -0800

    tut-4 hide elements tut

--- js/exam4.js
+++ js/exam4.js
@@ -1,6 +1,12 @@
 var app4 = angular.module('app4',[]);
 
 app4.controller('eventCtrl',function($scope){
-	$scope.disableButton = true;
-	
+	$scope.dayTimeButton = true;
+	$scope.capitals = [
+		{"City" : "Montgomery" , "State": "Alabama"},
+		{"City" : "Juneau" , "State": "Alaska"},
+		{"City" : "Phoenix" , "State": "Arizona"},
+		{"City" : "Little Rock" , "State": "Arkansas"}
+	];
+
 });
